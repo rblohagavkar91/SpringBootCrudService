7a5b337ad6bdee38795fd01d54eedcff2f01f4c2
RXXAND <RXXAND@RD0063252.global.scd.scania.com>
2019-09-14 11:51:55 +0200

    code modified for user service

--- src/main/java/com/rahul/springboot/rest/UserDirectoryApplication.java
+++ src/main/java/com/rahul/springboot/rest/UserDirectoryApplication.java
@@ -2,12 +2,21 @@ package com.rahul.springboot.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UserDirectoryApplication {
+public class UserDirectoryApplication  extends SpringBootServletInitializer {
+	
+	 @Override
+	   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	      return application.sources(UserDirectoryApplication.class);
+	   }
+	 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SpringApplication.run(UserDirectoryApplication.class, args);
 	}
+	
 }
 
